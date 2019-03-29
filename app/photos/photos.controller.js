228362017482a572cae26530e215a4a915013564
Havrylei <havrylei.petro@gmail.com>
2019-03-29 18:30:27 +0200

    finish with photos

--- app/photos/photos.controller.js
+++ app/photos/photos.controller.js
@@ -5,7 +5,39 @@ app.controller("PhotosCtrl", function($scope, $rootScope, $firebaseArray) {
     // Public Properties
     $scope.photos = $firebaseArray(photosRepository);
     $scope.isEdit = false;
-    $scope.photo = {};
+    $scope.currentPhoto = {};
     
     // Public Methods
+    $scope.savePhoto = function() {
+        if(!$scope.isEdit) {
+            photosRepository.push($scope.currentPhoto);
+        } else {
+            photosRepository.child($scope.currentPhoto.$id).update({
+                title: $scope.currentPhoto.title,
+                urlSmall: $scope.currentPhoto.urlSmall,
+                urlLarge: $scope.currentPhoto.urlLarge
+            });
+        }
+
+        $scope.clear();
+    }
+
+    $scope.editPhoto = function(photo) {
+        $scope.currentPhoto = Object.assign({}, photo);
+        $scope.isEdit = true;
+        $scope.photosForm.$pristine = true;
+    }
+
+    $scope.clear = function() {
+        $scope.currentPhoto = {};
+        $scope.isEdit = false;
+
+        $scope.photosForm.$pristine = true;
+    }
+
+    $scope.deletePhoto = function() {
+        photosRepository.child($scope.currentPhoto.$id).remove();
+
+        $scope.clear();
+    }
 });
